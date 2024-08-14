b56ee4a3eb510d05dd64da17e729b5b5bf904d31
Li Sun <forinec@googlemail.com>
2024-08-14 22:57:55 +0800

    color scheme tryout

--- shell/wezterm.lua
+++ shell/wezterm.lua
@@ -12,8 +12,9 @@ config.font_size = 14.5
 config.initial_cols = 162
 config.initial_rows = 66
 -- config.color_scheme = "Monokai (terminal.sexy)"
-config.color_scheme = "Tokyo Night (Gogh)"
 -- config.color_scheme = "Gruvbox Dark (Gogh)"
+-- config.color_scheme = "Tokyo Night (Gogh)"
+config.color_scheme = "Vs Code Dark+ (Gogh)"
 config.enable_tab_bar = false
 config.window_background_opacity = 0.975
 
